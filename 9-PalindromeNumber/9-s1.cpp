e92872f3674f672264216558aec5ff56a740344b
Lulu Wu <sky110423@gmail.com>
2024-02-16 23:35:05 -0800

    9. Palindrome Number

---
+++ 9-PalindromeNumber/9-s1.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Solution
+{
+public:
+    bool isPalindrome(int x)
+    {
+        if (x < 0)
+        {
+            return false;
+        }
+        else if (x < 10)
+        {
+            return true;
+        }
+
+        return testPalindrome(std::to_string(x), 0, std::to_string(x).size() - 1);
+    }
+
+    bool testPalindrome(string palindrome, size_t left, size_t right)
+    {
+        if (left >= palindrome.size() / 2)
+        {
+            if (palindrome[left] == palindrome[right])
+            {
+                return true;
+            }
+        }
+
+        return palindrome[left] == palindrome[right] && testPalindrome(palindrome, left + 1, right - 1);
+    }
+};
+
+int main()
+{
+    // declaraction
+    Solution s;
+    int num;
+
+    // input
+    cout << "Enter an integer: ";
+    cin >> num;
+
+    // processing and output
+    cout << "\nResult: ";
+    if (s.isPalindrome(num))
+    {
+        cout << num << " is a Palindrome Number.\n";
+    }
+    else
+    {
+        cout << num << " is not a Palindrome Number.\n";
+    }
+
+    return 0;
+}
