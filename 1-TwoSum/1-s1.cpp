1d14fb4712c2525a8539917f37f2f96ce6b30bc2
Lulu Wu <sky110423@gmail.com>
2024-02-19 23:45:14 -0800

    Format

--- 1-TwoSum/1-s1.cpp
+++ 1-TwoSum/1-s1.cpp
@@ -3,60 +3,49 @@
 
 using namespace std;
 
-class Solution
-{
-public:
-    vector<int> twoSum(vector<int> &nums, int target)
-    {
-        int len = nums.size();
-        for (int i{0}; i < len - 1; i++)
-        {
-            for (int j{i + 1}; j < len; j++)
-            {
-                if (nums[i] + nums[j] == target)
-                {
-                    return {i, j};
-                }
-            }
+class Solution {
+ public:
+  vector<int> twoSum(vector<int> &nums, int target) {
+    int len = nums.size();
+    for (int i{0}; i < len - 1; i++) {
+      for (int j{i + 1}; j < len; j++) {
+        if (nums[i] + nums[j] == target) {
+          return {i, j};
         }
-        return {};
+      }
     }
+    return {};
+  }
 };
 
-int main()
-{
-    // declaration
-    vector<int> nums, result;
-    int input, target;
-    Solution s;
+int main() {
+  // declaration
+  vector<int> nums, result;
+  int input, target;
+  Solution s;
 
-    // input
-    cout << "Enter list numbers, -1 to stop: \n> ";
-    while (cin >> input)
-    {
-        if (input == -1)
-        {
-            break;
-        }
-        cout << "> ";
-        nums.push_back(input);
+  // input
+  cout << "Enter list numbers, -1 to stop: \n> ";
+  while (cin >> input) {
+    if (input == -1) {
+      break;
     }
+    cout << "> ";
+    nums.push_back(input);
+  }
 
-    cout << "Enter the target: ";
-    cin >> target;
+  cout << "Enter the target: ";
+  cin >> target;
 
-    // processing
-    result = s.twoSum(nums, target);
+  // processing
+  result = s.twoSum(nums, target);
 
-    // output
-    if (!result.empty())
-    {
-        cout << "\nResult: [" << result[0] << ", " << result[1] << "]" << endl;
-    }
-    else
-    {
-        cout << "No two elements found that sum up to the target." << endl;
-    }
+  // output
+  if (!result.empty()) {
+    cout << "\nResult: [" << result[0] << ", " << result[1] << "]" << endl;
+  } else {
+    cout << "No two elements found that sum up to the target." << endl;
+  }
 
-    return 0;
+  return 0;
 }
