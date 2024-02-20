e91f2451097d2d6a26567156904a78e442c7b2a8
Lulu Wu <sky110423@gmail.com>
2024-02-19 23:45:05 -0800

    More solutions

---
+++ 9-PalindromeNumber/9-s3.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+
+using namespace std;
+
+class Solution {
+ public:
+  bool isPalindrome(int x) {
+    if (x < 0)
+      return false;
+    else if (x < 10)
+      return true;
+
+    if (x < 0) {
+      return 0;
+    } else if (x < 10) {
+      return 1;
+    }
+
+    long temp = x;
+    long reversed = 0;
+    int dig;
+
+    while (temp != 0) {
+      dig = temp % 10;
+      reversed = reversed * 10 + dig;
+      temp /= 10;
+    }
+
+    return x == reversed;
+  }
+};
+
+int main() {
+  // declaraction
+  Solution s;
+  int num;
+
+  // input
+  cout << "Enter an integer: ";
+  cin >> num;
+
+  // processing and output
+  cout << "\nResult: ";
+  if (s.isPalindrome(num)) {
+    cout << num << " is a Palindrome Number.\n";
+  } else {
+    cout << num << " is not a Palindrome Number.\n";
+  }
+
+  return 0;
+}
