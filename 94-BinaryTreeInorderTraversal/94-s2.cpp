aa2e5439e3da1a32cefe97730ab362a90a196fa6
Lulu Wu <sky110423@gmail.com>
2024-03-24 23:38:03 -0700

    94. Binary Tree Inorder Traversal

---
+++ 94-BinaryTreeInorderTraversal/94-s2.cpp
@@ -0,0 +1,94 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+struct TreeNode {
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode *left, TreeNode *right)
+      : val(x), left(left), right(right) {}
+};
+
+class Solution {
+ public:
+  vector<int> inorderTraversal(TreeNode *root) {
+    vector<int> res;
+    vector<TreeNode *> stack;
+    TreeNode *curr = root;
+
+    while (curr != nullptr || !stack.empty()) {
+      while (curr != nullptr) {
+        stack.push_back(curr);
+        curr = curr->left;
+      }
+      curr = stack.back();
+      stack.pop_back();
+      res.push_back(curr->val);
+      curr = curr->right;
+    }
+
+    return res;
+  }
+};
+
+TreeNode *constructBinaryTree(vector<int> nums) {
+  if (nums.empty()) {
+    return nullptr;
+  }
+  vector<TreeNode *> nodes;
+  for (int i = 0; i < nums.size(); i++) {
+    if (nums[i] != -1) {
+      nodes.push_back(new TreeNode(nums[i]));
+    } else {
+      nodes.push_back(nullptr);
+    }
+  }
+
+  int leftIndex = 1, rightIndex = 2;
+  for (int i = 0; i < nums.size(); i++) {
+    if (nums[i] != -1) {
+      if (leftIndex < nums.size()) {
+        nodes[i]->left = nodes[leftIndex];
+      }
+      if (rightIndex < nums.size()) {
+        nodes[i]->right = nodes[rightIndex];
+      }
+
+      leftIndex += 2;
+      rightIndex += 2;
+    }
+  }
+  return nodes[0];
+}
+
+int main() {
+  // declaration
+  Solution s = Solution();
+  vector<int> nums, result;
+  int num;
+
+  // input
+  cout << ("Enter numbers, EOF to stop: \n> ");
+  while (cin >> num) {
+    nums.push_back(num);
+    cout << "> ";
+  }
+
+  TreeNode *root = constructBinaryTree(nums);
+
+  // processing
+  result = s.inorderTraversal(root);
+
+  // output
+  cout << "\n\nResult:\n";
+  for (int n : result) {
+    cout << n << " ";
+  }
+  cout << endl << endl;
+
+  return 0;
+}
