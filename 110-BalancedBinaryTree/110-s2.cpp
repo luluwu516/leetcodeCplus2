1d5c317db5bb57defa8633cf389376a7440f93a0
Lulu Wu <sky110423@gmail.com>
2024-04-09 10:26:34 -0700

    110-more solutions

---
+++ 110-BalancedBinaryTree/110-s2.cpp
@@ -0,0 +1,94 @@
+#include <stdlib.h>
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+struct TreeNode {
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode *left, TreeNode *right)
+      : val(x), left(left), right(right) {}
+};
+
+class Solution {
+ public:
+  bool isBalanced(TreeNode *root) {
+    bool res = true;
+    check(root, res);
+    return res;
+  }
+  int check(TreeNode *root, bool &res) {
+    if (root == NULL) {
+      return 0;
+    }
+    int depthLeft = check(root->left, res);
+    int depthRight = check(root->right, res);
+
+    if (abs(depthLeft - depthRight) > 1) {
+      res = false;
+    }
+    return max(depthLeft, depthRight) + 1;
+  }
+};
+
+TreeNode *constructBinaryTree(vector<int> nums) {
+  if (nums.empty()) {
+    return nullptr;
+  }
+  vector<TreeNode *> nodes;
+  for (int i = 0; i < nums.size(); i++) {
+    if (nums[i] != -1) {
+      nodes.push_back(new TreeNode(nums[i]));
+    } else {
+      nodes.push_back(nullptr);
+    }
+  }
+
+  int leftIndex = 1, rightIndex = 2;
+  for (int i = 0; i < nums.size(); i++) {
+    if (nums[i] != -1) {
+      if (leftIndex < nums.size()) {
+        nodes[i]->left = nodes[leftIndex];
+      }
+      if (rightIndex < nums.size()) {
+        nodes[i]->right = nodes[rightIndex];
+      }
+
+      leftIndex += 2;
+      rightIndex += 2;
+    }
+  }
+  return nodes[0];
+}
+
+int main() {
+  // declaration
+  Solution s = Solution();
+  vector<int> nums;
+  int num;
+  bool result;
+
+  // input
+  cout << ("Enter numbers, EOF to stop: \n> ");
+  while (cin >> num) {
+    nums.push_back(num);
+    cout << "> ";
+  }
+
+  TreeNode *root = constructBinaryTree(nums);
+
+  // processing
+  result = s.isBalanced(root);
+
+  // output
+  cout << "\n\nResult:\n";
+  cout << "The tree is " << (result ? "balanced" : "not balanced") << endl
+       << endl;
+
+  return 0;
+}
