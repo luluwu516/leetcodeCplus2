cfdd83ec73d8589fa0454571b525323242b88f60
Lulu Wu <sky110423@gmail.com>
2024-05-31 11:27:30 -0700

    203. Remove Linked List Elements

---
+++ 203-RemoveLinkedListElements/203-s2.cpp
@@ -0,0 +1,94 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+struct ListNode {
+  int val;
+  ListNode* next;
+  ListNode() : val(0), next(nullptr) {}
+  ListNode(int x) : val(x), next(nullptr) {}
+  ListNode(int x, ListNode* next) : val(x), next(next) {}
+};
+
+class Solution {
+ public:
+  ListNode* removeElements(ListNode* head, int val) {
+    if (head == nullptr) {
+      return head;
+    }
+    ListNode* dummy = new ListNode(0, head);
+    ListNode* curr = dummy;
+
+    while (curr->next != nullptr) {
+      if (curr->next->val == val) {
+        curr->next = curr->next->next;
+      } else {
+        curr = curr->next;
+      }
+    }
+
+    return dummy->next;
+  }
+};
+
+// function prototypes
+ListNode* constructLinkList(vector<int> nums);
+void printLinkList(ListNode* head);
+
+int main() {
+  // declaration
+  Solution s = Solution();
+  int num;
+  vector<int> nums;
+  struct ListNode *head, *res;
+
+  // input
+  cout << "\nEnter numbers to build a Linked List, -1 to stop: \n> ";
+  cin >> num;
+  while (num != -1) {
+    nums.push_back(num);
+    cout << "> ";
+    cin >> num;
+  }
+
+  cout << "\nEnter a value to remove: ";
+  cin >> num;
+
+  head = constructLinkList(nums);
+
+  // processing and output
+  cout << "\nResult:\n";
+  cout << "Original: ";
+  printLinkList(head);
+
+  res = s.removeElements(head, num);
+  cout << "Revised : ";
+  printLinkList(res);
+
+  return 0;
+}
+
+ListNode* constructLinkList(vector<int> nums) {
+  if (nums.empty()) {
+    return nullptr;
+  }
+  ListNode* head = new ListNode(nums[0]);
+  ListNode* curr = head;
+
+  for (int i = 1; i < nums.size(); i++) {
+    curr->next = new ListNode(nums[i]);
+    curr = curr->next;
+  }
+
+  return head;
+}
+
+void printLinkList(ListNode* head) {
+  ListNode* curr = head;
+  while (curr != nullptr) {
+    cout << curr->val << " ";
+    curr = curr->next;
+  }
+  cout << endl;
+}
